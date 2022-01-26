354af3dbad09512d875e6b32ac798365fa433594
pfwd <hello@websomatic.co.uk>
2022-01-26 23:47:09 +0000

    Adding blog

---
+++ src/pages/blog/index.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+import Layout from "../../components/layout"
+import {graphql} from "gatsby";
+import PostList from "../../components/postList";
+
+const BlogPage = ({data}) => (
+    <Layout>
+        <h2>Latest posts</h2>
+        { data.allMarkdownRemark.edges.map (({ node }) => (<>
+            <PostList data={node.frontmatter} key={node.id}/>
+        </>))}
+
+    </Layout>
+)
+
+export default BlogPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(filter: {frontmatter: {type: {eq: "blog"}}}, sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            slug
+            title
+          }
+        }
+      }
+    }
+  }
+`
