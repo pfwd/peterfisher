8026a77c88db8e9ced0f3597f42c3adc67bfdfc0
pfwd <hello@websomatic.co.uk>
2022-02-18 21:20:03 +0000

    Adding slides

---
+++ src/pages/slides/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Layout from "../../components/layout";
+import SEO from "../../components/seo"
+
+const IndexPage = () => {
+
+
+    return (
+        <Layout>
+            <SEO title="Peter Fisher PHP Contractor"/>
+            <ul>
+                <li><a href={'/slides/code-with-confidence-using-phpstan.html'}>Code with confidence using PHPStan</a>
+                </li>
+            </ul>
+
+        </Layout>
+    )
+}
+export default IndexPage
